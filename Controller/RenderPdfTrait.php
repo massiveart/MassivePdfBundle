79e2b6280aa30fe4579e938d64580bf4f119ce38
Alexander Schranz <alexander@sulu.io>
2019-05-13 11:31:42 +0200

    Make RenderPdfTrait work without container (#12)

--- Controller/RenderPdfTrait.php
+++ Controller/RenderPdfTrait.php
@@ -12,24 +12,44 @@
 namespace Massive\Bundle\PdfBundle\Controller;
 
 use Massive\Bundle\PdfBundle\Pdf\PdfFactory;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Tests\Controller\ContainerAwareController;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 trait RenderPdfTrait
 {
+    /**
+     * @var PdfFactory
+     */
+    protected $pdfFactory;
+
+    protected function getPdfFactory()
+    {
+        if (!$this->pdfFactory) {
+            if (!$this instanceof Controller) {
+                throw new \RuntimeException('The pdfFactory service need to be set to be used.');
+            }
+
+            $this->pdfFactory = $this->get('massive_pdf.pdf_factory');
+        }
+
+        return $this->pdfFactory;
+    }
+
     protected function renderPdf($template, $parameters = [], $requestFormat = 'pdf', Response $response = null)
     {
-        /** @var PdfFactory $pdfFactory */
-        $pdfFactory = $this->get('massive_pdf.pdf_factory');
-
         if (!$response) {
             $response = new Response();
         }
 
+        $parameters['request_format'] = $requestFormat;
+
         if ('pdf' === $requestFormat) {
-            $content = $pdfFactory->create($template, array_merge($parameters, ['request_format' => 'pdf']));
+            $content = $this->pdfFactory->create($template, $parameters);
             $response->headers->set('Content-Type', 'application/pdf');
         } else {
-            $content = $this->renderView($template, array_merge($parameters, ['request_format' => 'html']));
+            $content = $this->pdfFactory->generateHtml($template, $parameters);
         }
 
         $response->setContent($content);
